6fdf07d0062513fd2c7b39efb17a0ef4fa852fe0
Shohan <ashraful.mallik100@gmail.com>
2019-03-19 15:32:34 -0400

    Some further CSS changes

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -10,7 +10,7 @@ render(){
     className = "hvr-shadow"  
     style={{ width: 300 }}
     cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-    actions={[<Icon type="github" />]}
+    actions={[<Icon type="github"/>]}
   >
     <Meta
       avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
