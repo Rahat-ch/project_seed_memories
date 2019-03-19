147a94fafaaad60d2f9e043b9da35ea59cab2d10
Shohan <ashraful.mallik100@gmail.com>
2019-03-18 23:46:21 -0400

    Updates to log-in page. Added ds_store to gitignore.

---
+++ client/src/components/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+class Buttons extends React.Component{
+    render(){
+    return(
+        <div >
+
+                    <img id="logo" src={require('../assets/images/psLogo.png')} />
+                    <br></br>
+                    <span id="slogan">JUMP OUT OF THE TUTORIAL HOLE AND <br></br> JUMP INTO PROJECT SEED!</span>
+                    <br></br>
+                    <Button id="githBtn" type="primary">MAKE A GITHUB ACCOUNT</Button>
+                    <br></br>
+                    <Button id="loginBtn"> LOG IN WITH GITHUB</Button>
+        </div>
+        )
+    }
+
+    
+}
+
+export default Buttons;
+  
